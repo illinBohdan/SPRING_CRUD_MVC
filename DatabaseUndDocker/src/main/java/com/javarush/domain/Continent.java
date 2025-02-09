4ec7d12c65b10b7ed41b833652ac546ebb4b576b
BohdanLazarchuk <blazarchuk@gmail.com>
2025-02-09 12:15:22 +0100

    Comit and push all

---
+++ DatabaseUndDocker/src/main/java/com/javarush/domain/Continent.java
@@ -0,0 +1,14 @@
+package com.javarush.domain;
+
+import jakarta.persistence.Enumerated;
+
+public enum Continent {
+    
+    ASIA,
+    EUROPE,
+    NORTH_AMERICA,
+    AFRICA,
+    OCEANIA,
+    ANTARCTICA,
+    SOUTH_AMERICA
+}
